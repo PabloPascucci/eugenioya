1b64c8b1c8e1b7967fc57565a1d4f7e4654181d8
PabloPascucci <pascuccipablo20@gmail.com>
2025-11-18 23:38:45 -0300

    Update server_.php

--- server_.php
+++ server_.php
@@ -3,15 +3,9 @@
 
     // ==>> Credenciales en localhost <<==
 
-    $server = "localhost";
-    $user = "root";
-    $password = "";
-    $db_name = "eugenioya";
+
 
     // ==>> Credenciales en "Producción" <<==
 
-    // $server = "localhost";
-    // $user = "u320308767_genio";
-    // $password = "X9sTcrz9Dg;";
-    // $db_name = "u320308767_eugenioYa";
+
 ?>
